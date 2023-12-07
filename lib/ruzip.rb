ed8766171a7ae23b65be675fab748b5bc4818fbb
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-08 00:24:37 +0900

    Initial commit
        % bundle gem --ext=rust --no-mit --test=test-unit --ci=gitlab ruzip

---
+++ lib/ruzip.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "ruzip/version"
+require_relative "ruzip/ruzip"
+
+module Ruzip
+  class Error < StandardError; end
+  # Your code goes here...
+end
