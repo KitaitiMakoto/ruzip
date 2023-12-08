3c9c28e6f5089021c15e044f984374e944829708
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-09 00:16:12 +0900

    Remove boierplate function

--- ext/ruzip/src/lib.rs
+++ ext/ruzip/src/lib.rs
@@ -1,12 +1,7 @@
 use magnus::{function, prelude::*, Error, Ruby};
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
-}
-
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("RuZip")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
     Ok(())
 }
