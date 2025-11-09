5ac376306c72fc9616c7d04a43bd36052ba2caa9
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-11-09 14:02:31 +0900

    Add test for Writer to write file

--- test/test_writer.rb
+++ test/test_writer.rb
@@ -1,5 +1,6 @@
 require_relative "helper"
 require "tmpdir"
+require "archive/zip"
 
 class TestWriter < Test::Unit::TestCase
   test "new with path" do
@@ -19,6 +20,24 @@ class TestWriter < Test::Unit::TestCase
       assert_instance_of RuZip::Archive, archive
       assert_raise do
         writer.finish
+      end
+    end
+  end
+
+  test "add file" do
+    Dir.mktmpdir do |dir|
+      path = File.join(dir, "output.zip")
+      writer = RuZip::Writer.new(path)
+      inner_path = "hello.txt"
+      writer.start_file(inner_path)
+      content = "Hello, World!"
+      writer.write content
+      writer.finish
+
+      Archive::Zip.open path do |archive|
+        file = archive.each.first
+        assert_equal inner_path, file.zip_path
+        assert_equal content, file.file_data.read(content.length)
       end
     end
   end
